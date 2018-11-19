4fb49272852fa6b0d552db8ffbe25f509d8364ae
Serkan TAS <tasserkan93@gmail.com>
2018-11-19 05:19:24 +0300

    initial projects

---
+++ CaseProject/Form1.cs
@@ -0,0 +1,45 @@
+﻿using CaseProject.Robots;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CaseProject
+{
+    public partial class Form1 : Form
+    {
+        private Robot r1;
+        private Robot r2;
+        private int X;
+        private Point p;
+        public Form1()
+        {
+            InitializeComponent();
+            r1 = new Robot(new Size(30,60),new Point(0,0));
+            r2 = new Robot();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            r1.BackColor = Color.Aqua;
+            
+            Controls.Add(r1);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            p = r1.Location;
+            p.X += 5;
+            r1.Location = p;
+        }
+
+
+       
+    }
+}
---
+++ CaseProject/Robots/Robot.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CaseProject.Robots
+{
+    class Robot : Panel
+    {
+       
+        public Robot() { }
+        public Robot(Size size,Point point) {
+            this.Size = size;
+            this.Location = point;
+        }
+    }
+}
